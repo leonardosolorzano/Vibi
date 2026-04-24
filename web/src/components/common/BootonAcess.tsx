0787ba68e9b9191de8ddcee4dc532e79996752a9
leo <ivanpacheco.solorzano@gmail.com>
2026-04-23 21:15:34 -0500

    start new frontend

--- web/src/components/common/BootonAcess.tsx
+++
@@ -1,9 +0,0 @@
-const BootonAcess = () => {
-  return (
-    <button className="px-4 py-2 bg-pink-700 text-white rounded-md hover:bg-pink-600 transition-colors">
-      Acceder
-    </button>
-  );
-}
-
-export default BootonAcess;
